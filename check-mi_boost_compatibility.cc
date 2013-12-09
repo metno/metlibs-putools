307b468032b24b53d772219a15be75e1d40f6fe0
Alexander Bürger <alexander.buerger@met.no>
2013-12-09 12:01:23 +0000

    added mi_boost_compatibility header and tests

---
+++ check-mi_boost_compatibility.cc
@@ -0,0 +1,99 @@
+
+#define MI_BOOST_COMPATIBILITY_FORCE_SHARED_PTR
+#define MI_BOOST_COMPATIBILITY_FORCE_MAP_ADAPTORS
+#include "mi_boost_compatibility.hh"
+
+#include <boost/foreach.hpp>
+#include <map>
+#include <string>
+
+#include <gtest/gtest.h>
+
+namespace /*anonymous*/ {
+struct Hello
+{
+  static int what;
+  Hello()
+    { what = 0; }
+  Hello(int)
+    { what = 1; }
+  Hello(const char*)
+    { what = 2; }
+};
+
+int Hello::what = -1;
+} // anonymous namespace
+
+TEST(mi_boost_compatibility_test, make_shared)
+{
+    miutil::make_shared<Hello>("world");
+    EXPECT_EQ(2, Hello::what);
+    miutil::make_shared<Hello>();
+    EXPECT_EQ(0, Hello::what);
+    miutil::make_shared<Hello>(2);
+    EXPECT_EQ(1, Hello::what);
+}
+
+TEST(mi_boost_compatibility_test, map_keys_values)
+{
+  const size_t N = 4;
+  const int keys[N] = { 1, 3, 5, 7 };
+  const std::string values[] = { "one", "three", "five", "seven" };
+
+  typedef std::map<int, std::string> mymap_t;
+  mymap_t mymap;
+  for (size_t i=0; i<N; ++i)
+    mymap[keys[i]] = values[i];
+  const mymap_t mymap_c(mymap);
+  
+  int i1 = 0;
+  BOOST_FOREACH(int k, miutil::adaptors::keys(mymap)) {
+    EXPECT_EQ(keys[i1++], k);
+  }
+  EXPECT_EQ(N, i1);
+  
+  int i2 = 0;
+  BOOST_FOREACH(int k, miutil::adaptors::keys(mymap_c)) {
+    EXPECT_EQ(keys[i2++], k);
+  }
+  EXPECT_EQ(N, i2);
+  
+  int i3 = N;
+  BOOST_REVERSE_FOREACH(int k, miutil::adaptors::keys(mymap)) {
+    EXPECT_EQ(keys[--i3], k);
+  }
+  EXPECT_EQ(0, i3);
+  
+  int i4 = 0;
+  BOOST_FOREACH(const std::string& v, miutil::adaptors::values(mymap)) {
+    EXPECT_EQ(values[i4++], v);
+  }
+  EXPECT_EQ(N, i4);
+  
+  int i5 = N;
+  BOOST_REVERSE_FOREACH(const std::string& v, miutil::adaptors::values(mymap)) {
+    EXPECT_EQ(values[--i5], v);
+  }
+  EXPECT_EQ(0, i5);
+
+  typedef miutil::adaptors::map<mymap_t>::values mymap_values_t;
+  mymap_values_t mv(mymap);
+  for (mymap_values_t::reverse_iterator it=mv.rbegin(); it != mv.rend(); ++it) {
+    *it += " fish";
+  }
+
+  int i6 = 0;
+  typedef miutil::adaptors::map<const mymap_t>::keys mymap_keys_t;
+  const mymap_keys_t mk1(mymap_c);
+  for (mymap_keys_t::const_iterator it=mk1.begin(); it != mk1.end(); ++it) {
+    EXPECT_EQ(keys[i6++], *it);
+  }
+  EXPECT_EQ(N, i6);
+
+  int i7 = 0;
+  const mymap_keys_t mk2(mymap);
+  for (mymap_keys_t::const_iterator it=mk2.begin(); it != mk2.end(); ++it) {
+    EXPECT_EQ(values[i7++] + " fish", mymap.at(*it));
+  }
+  EXPECT_EQ(N, i7);
+}
---
+++ mi_boost_compatibility.hh
@@ -0,0 +1,255 @@
+
+#ifndef MI_BOOST_COMPATIBILITY_HH
+#define MI_BOOST_COMPATIBILITY_HH 1
+
+#include <boost/shared_ptr.hpp>
+#include <boost/version.hpp>
+#include <iterator>
+
+#if BOOST_VERSION >= 104300 && !defined(MI_BOOST_COMPATIBILITY_FORCE_MAP_ADAPTORS)
+#include <boost/range/adaptor/map.hpp>
+#endif
+
+#if BOOST_VERSION >= 103900 && !defined(MI_BOOST_COMPATIBILITY_FORCE_SHARED_PTR)
+#include <boost/make_shared.hpp>
+namespace miutil {
+using boost::make_shared;
+}
+#else
+namespace miutil {
+template<class C>
+boost::shared_ptr<C> make_shared()
+{
+  return boost::shared_ptr<C>(new C);
+}
+
+template<class C, class A1>
+boost::shared_ptr<C> make_shared(const A1& a1)
+{
+  return boost::shared_ptr<C>(new C(a1));
+}
+
+template<class C, class A1, class A2>
+boost::shared_ptr<C> make_shared(const A1& a1, const A2& a2)
+{
+  return boost::shared_ptr<C>(new C(a1, a2));
+}
+
+template<class C, class A1, class A2, class A3>
+boost::shared_ptr<C> make_shared(const A1& a1, const A2& a2, const A3& a3)
+{
+  return boost::shared_ptr<C>(new C(a1, a2, a3));
+}
+
+template<class C, class A1, class A2, class A3, class A4>
+boost::shared_ptr<C> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4)
+{
+  return boost::shared_ptr<C>(new C(a1, a2, a3, a4));
+}
+
+template<class C, class A1, class A2, class A3, class A4, class A5>
+boost::shared_ptr<C> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5)
+{
+  return boost::shared_ptr<C>(new C(a1, a2, a3, a4, a5));
+}
+
+template<class C, class A1, class A2, class A3, class A4, class A5, class A6>
+boost::shared_ptr<C> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6)
+{
+  return boost::shared_ptr<C>(new C(a1, a2, a3, a4, a5, a6));
+}
+
+template<class C, class A1, class A2, class A3, class A4, class A5, class A6, class A7>
+boost::shared_ptr<C> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7)
+{
+  return boost::shared_ptr<C>(new C(a1, a2, a3, a4, a5, a6, a7));
+}
+
+template<class C, class A1, class A2, class A3, class A4, class A5, class A6, class A7, class A8>
+boost::shared_ptr<C> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8)
+{
+  return boost::shared_ptr<C>(new C(a1, a2, a3, a4, a5, a6, a7, a8));
+}
+
+template<class C, class A1, class A2, class A3, class A4, class A5, class A6, class A7, class A8, class A9>
+boost::shared_ptr<C> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8, const A9& a9)
+{
+  return boost::shared_ptr<C>(new C(a1, a2, a3, a4, a5, a6, a7, a8, a9));
+}
+
+} // namespace miutil
+#endif
+
+// ================================================================================
+namespace miutil {
+
+namespace adaptors {
+
+namespace detail {
+template<class M>
+struct map_get_key {
+  typedef typename M::value_type map_value_type;
+  typedef typename M::key_type   value_type;
+  const value_type& operator()(const map_value_type& v) const
+    { return v.first; }
+};
+
+template<class M>
+struct map_get_value {
+  typedef typename M::value_type  map_value_type;
+  typedef typename M::mapped_type value_type;
+  const value_type& operator()(const map_value_type& v) const
+    { return v.second; }
+  value_type& operator()(map_value_type& v)
+    { return v.second; }
+};
+
+template<class M, class G>
+struct map_wrapper
+{
+  typedef typename M::iterator               m_iterator;
+  typedef typename M::reverse_iterator       m_reverse_iterator;
+  typedef typename M::const_iterator         m_const_iterator;
+  typedef typename M::const_reverse_iterator m_const_reverse_iterator;
+  typedef typename G::value_type             g_value_type;
+
+  template<class I, class V>
+  struct wrap_iterator : public std::iterator<std::bidirectional_iterator_tag, V>
+  {
+    typedef V value_type;
+    typedef value_type* pointer;
+    typedef value_type& reference;
+
+    wrap_iterator(const wrap_iterator& o)
+      : iter(o.iter) { }
+
+    wrap_iterator& operator=(const wrap_iterator& o)
+      { iter = o.iter; return *this; }
+
+    bool operator==(const wrap_iterator& o) const
+      { return iter == o.iter; }
+
+    bool operator!=(const wrap_iterator& o) const
+      { return iter != o.iter; }
+
+    wrap_iterator& operator++()
+      { ++iter; return *this; }
+
+    wrap_iterator& operator++(int)
+      { iter++; return *this; }
+        
+    wrap_iterator& operator--()
+      { --iter; return *this; }
+
+    wrap_iterator& operator--(int)
+      { iter--; return *this; }
+        
+    reference operator*()
+      { return G()(*iter); }
+
+    pointer operator->()
+      { return &G()(*iter); }
+
+  private:
+    friend class map_wrapper;
+    wrap_iterator(const I& i)
+      : iter(i) { }
+        
+  private:
+    I iter;
+  };
+
+  template<class I, class NCI, class V>
+  struct wrap_const_iterator : public wrap_iterator<I, V>
+  {
+    wrap_const_iterator(const wrap_const_iterator& o)
+      : wrap_iterator<I,V>(o) { }
+
+    wrap_const_iterator(const wrap_iterator<I,V>& o)
+      : wrap_iterator<I,V>(o.iter) { }
+
+  private:
+    friend class map_wrapper;
+    wrap_const_iterator(const NCI& i)
+      : wrap_iterator<I,V>(i) { }
+  };
+
+  typedef wrap_iterator<m_iterator, g_value_type> iterator;
+  typedef wrap_const_iterator<m_const_iterator, m_iterator, const g_value_type> const_iterator;
+
+  typedef wrap_iterator<m_reverse_iterator, g_value_type> reverse_iterator;
+  typedef wrap_const_iterator<m_const_reverse_iterator, m_reverse_iterator, const g_value_type> const_reverse_iterator;
+
+  map_wrapper(M& m)
+    : m_wrapped(m) { }
+
+  iterator begin()
+    { return iterator(m_wrapped.begin()); }
+
+  iterator end()
+    { return iterator(m_wrapped.end()); }
+    
+  const_iterator begin() const
+    { return const_iterator(m_wrapped.begin()); }
+
+  const_iterator end() const
+    { return const_iterator(m_wrapped.end()); }
+    
+
+  reverse_iterator rbegin()
+    { return reverse_iterator(m_wrapped.rbegin()); }
+
+  reverse_iterator rend()
+    { return reverse_iterator(m_wrapped.rend()); }
+    
+  const_reverse_iterator rbegin() const
+    { return const_reverse_iterator(m_wrapped.rbegin()); }
+
+  const_reverse_iterator rend() const
+    { return const_reverse_iterator(m_wrapped.rend()); }
+    
+private:
+  M& m_wrapped;
+};
+
+} // namespace detail
+
+template<class M>
+struct map {
+  typedef detail::map_wrapper< M, detail::map_get_key<M>   > keys;
+  typedef detail::map_wrapper< M, detail::map_get_value<M> > values;
+};
+
+#if BOOST_VERSION >= 104300 && !defined(MI_BOOST_COMPATIBILITY_FORCE_MAP_ADAPTORS)
+using boost::adaptors::keys;
+using boost::adaptors::values;
+#else
+template<class M>
+typename map<M>::keys keys(M& m)
+{
+  return typename map<M>::keys(m);
+}
+
+template<class M>
+typename map<const M>::keys keys(const M& m)
+{
+  return typename map<const M>::keys(m);
+}
+
+template<class M>
+typename map<M>::values values(M& m)
+{
+  return typename map<M>::values(m);
+}
+
+template<class M>
+typename map<const M>::values values(const M& m)
+{
+  return typename map<const M>::values(m);
+}
+#endif // boost with map adaptors
+} // namespace adaptors
+
+} // namespace miutil
+
+#endif /* MI_BOOST_COMPATIBILITY_HH */
