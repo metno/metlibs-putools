49689ab8cbf6693b9eea1af4bdcb88df4b75cf9b
Alexander Bürger <alexander.buerger@met.no>
2016-03-10 12:34:07 +0000

    cleanup miSort
     * drop Swap, should use std::swap
     * implement Bsearch with std::binary_search

--- src/miSort.h
+++ src/miSort.h
@@ -1,9 +1,7 @@
 /*
   libpuTools - Basic types/algorithms/containers
-  
-  $Id$
 
-  Copyright (C) 2006 met.no
+  Copyright (C) 2006-2016 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -11,7 +9,7 @@
   0313 OSLO
   NORWAY
   email: diana@met.no
-  
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
@@ -21,7 +19,7 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-  
+
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -37,48 +35,32 @@
 #ifndef _miSort_h
 #define _miSort_h
 
+#include <algorithm>
 
-template<class T> void Swap(T& x, T& y)
-{
-  T tmp = x;
-  x = y;
-  y = tmp;
-}
+namespace miutil {
 
 template<class T> void Qsort(T* a, int l, int r)
 {
-  int i, z;
-
   if (l>=r)
     return;
 
-  Swap(a[l], a[(l+r)/2]);
-  z=l;
-  for (i=l+1; i<=r; i++)
-    if (a[i]<a[l])
-      Swap(a[++z],a[i]);
+  std::swap(a[l], a[(l+r)/2]);
+  int z = l;
+  for (int i=l+1; i<=r; i++)
+    if (a[i] < a[l])
+      std::swap(a[++z], a[i]);
 
-  Swap(a[l],a[z]);
-  Qsort(a,l,z-1);
-  Qsort(a,z+1,r);
+  std::swap(a[l], a[z]);
+  Qsort(a, l,z-1);
+  Qsort(a, z+1,r);
 }
 
-template<class T> bool Bsearch(T& key, T* a, int n)
+template<class T>
+bool Bsearch(const T& key, T* a, int n)
 {
-  int lo,hi,mi;
-
-  lo=0;
-  hi=n-1;
-  while (lo<=hi) {
-    mi=(lo+hi)/2;
-    if (key<a[mi])
-      hi=mi-1;
-    else if (key>a[mi])
-      lo=mi+1;
-    else
-      return true;
-  }
-  return false;
+  return std::binary_search(a, a+n, key);
 }
+
+} // namespace miutil
 
 #endif
